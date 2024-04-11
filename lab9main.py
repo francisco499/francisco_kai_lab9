5709a470dd2e48b8e97d7efa6ac27a23559e4849
francisco499 <85306614+francisco499@users.noreply.github.com>
2024-04-11 13:21:58 -0400

    Update lab9main.py

--- lab9main.py
+++ lab9main.py
@@ -1,2 +1,10 @@
-def encode(num):
-  return num
+def encode(password):
+  new_password = " "
+  for char in password:
+    new_char = str((int(char) + 3) % 10)
+    new_password += new_char
+  return new_password
+
+print(encoder("1234555"))
+
+    
