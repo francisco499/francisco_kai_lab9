21bb9a56be36eaa8670f6592b2653b33ac7e8e25
francisco499 <85306614+francisco499@users.noreply.github.com>
2024-04-11 13:52:06 -0400

    Update lab9main.py

--- lab9main.py
+++ lab9main.py
@@ -1,3 +1,20 @@
+menu = "Menu\n------------\n1. Encode\n 2. Decode\n 3. Quit
+def main():
+  while True:
+    print(menu)
+    option = int(imput("Please enter an option: ")
+    password = imput("Please enter the password to encode")
+    if option == 1:
+      encode(password)
+      print("Your password has been encoded and stored")
+    elif option == 2:
+      new_password = decode(password)
+      print(f"The encoded password is {encode(password)}, and the original password is {new_password}") 
+      
+    elif option == 3:
+      break
+    
+  
 def encode(password):
   new_password = " "
   for char in password:
@@ -6,4 +23,7 @@ def encode(password):
   return new_password
 
 
+
+  
+  
     
